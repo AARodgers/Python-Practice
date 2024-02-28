8efffdd1ace5b9f5fae3c322d5f506cdf4fffae9
Amanda Rodgers <aarodgerswork@gmail.com>
2024-02-27 17:57:16 -0700

    Add practice for number operations

---
+++ numbers.py
@@ -0,0 +1,50 @@
+#add
+sum = 2 + 3
+print(sum)
+
+#subtract
+difference = 5 - 2
+print(difference)
+
+#multiply
+product = 4 * 3
+print(product)
+
+#exponents
+result = 3**2
+print(result)
+
+#order of operation
+result2 = 2 + 3*4
+print(result2)
+
+#floats = any number with decimal points
+float = 0.1 + 0.1
+print(float)
+
+# sometimes you will get more decimal places than you thought
+float2 = 0.2 + 0.1
+print(float2)
+
+#if you divide to whole integers you will get a float
+# if you mix an integer and a float in other operations you will get a float
+quotient = 4/2
+print(quotient)
+
+# use underscores for large numbers
+universe_age = 14_000_000_000
+print(universe_age)
+
+#initialize a set of variables at the same time
+x, y, z = 0, 0, 0
+print(x, y, z)
+
+# write in all caps to tell other programmers to not change the value, but Python will let you
+#change it
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+MAX_CONNECTIONS =  MAX_CONNECTIONS + 1
+print(MAX_CONNECTIONS)
+
+favorite_number = 18
+print(f"My favorite number is {favorite_number}.")
