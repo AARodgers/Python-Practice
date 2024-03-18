eb865f0b4b7245fe6dffd1164c7d4d6ae4f563a2
Amanda Rodgers <aarodgerswork@gmail.com>
2024-03-18 15:57:57 -0600

    Add if_statement exercise file

---
+++ if_statements_exercises.py
@@ -0,0 +1,23 @@
+# if_statement exercises
+
+# if_statement
+
+# alien_color = 'green'
+# if alien_color == 'green':
+#     print("Player earns 5 points.")
+
+# alien_color = 'red'
+# if alien_color == 'green':
+#     print("Player earns no points.")
+
+alien_color = 'yellow'
+
+#if_else_statement
+
+# if alien_color == 'green':
+#     print("Player gets 5 points for shooting alien.")
+# else:
+#     print("Player earns 10 points.")
+
+#if_elif_else_statement
+
