59916eab1515c8f78f64264206ba210c6a67804c
Amanda Rodgers <aarodgerswork@gmail.com>
2024-03-19 17:02:09 -0600

    Add python scraping notes

--- scraping.py
+++ scraping.py
@@ -36,4 +36,27 @@ linked_text = scraped.article.h3.a.text
 linked_text
 #'A Light in the ...'
 
+# can use the attribute 'title' to get the full text
+linked_text = scraped.article.h3.a["title"]
+linked_text
+#'A Light in the Attic'
 
+#using find_all
+# find all objects with selector article and class product_pod
+items = scraped.find_all("article", class_="product_pod")
+
+# get the contents of the title attribute to the a selector
+for item in items:
+    print(item.h3.a["title"])
+# will give you a list of all of the book titles
+
+#will do the same as above, it an element has a unique trait
+title_links = scraped.find_all("a", title=True)
+
+for link in title_links:
+    print(link["title"])
+
+# find all links that are descendants of h3 and article
+scraped.select("article h3 a")
+
+#find all linds, h3, and articles, 
